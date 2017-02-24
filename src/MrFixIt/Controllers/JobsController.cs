ba6470c055aeac86fc1d05d2a6b679cf0722afa3
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-02-24 10:21:41 -0800

    add so that job can be claimed using ajax

--- src/MrFixIt/Controllers/JobsController.cs
+++ src/MrFixIt/Controllers/JobsController.cs
@@ -45,7 +45,7 @@ namespace MrFixIt.Controllers
             job.Worker = db.Workers.FirstOrDefault(i => i.UserName == User.Identity.Name);
             db.Entry(job).State = EntityState.Modified;
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return Json(job);
         }
     }
 }
